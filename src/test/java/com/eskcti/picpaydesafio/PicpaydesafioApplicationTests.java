d30a28213f59b8042b585d52380e61bd2dda69e4
eskokado <eskokado@gmail.com>
2023-09-02 08:06:38 -0300

    first commit

---
+++ src/test/java/com/eskcti/picpaydesafio/PicpaydesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eskcti.picpaydesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaydesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
