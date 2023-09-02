3e89ad124bf8665c9b6c07aca3170b2b933b2629
eskokado <eskokado@gmail.com>
2023-09-02 08:58:24 -0300

    feat: create model User

---
+++ src/main/java/com/eskcti/picpaydesafio/domain/user/User.java
@@ -0,0 +1,28 @@
+package com.eskcti.picpaydesafio.domain.user;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
