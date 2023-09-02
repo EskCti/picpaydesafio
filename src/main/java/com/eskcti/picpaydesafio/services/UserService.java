2ef582c0df2b056844659c1d383d7f8029abea17
eskokado <eskokado@gmail.com>
2023-09-02 12:29:44 -0300

    feat: create UserService

---
+++ src/main/java/com/eskcti/picpaydesafio/services/UserService.java
@@ -0,0 +1,45 @@
+package com.eskcti.picpaydesafio.services;
+
+import com.eskcti.picpaydesafio.domain.user.User;
+import com.eskcti.picpaydesafio.domain.user.UserType;
+import com.eskcti.picpaydesafio.dtos.UserDTO;
+import com.eskcti.picpaydesafio.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo Logista não está autorizado a realizar transação");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
+    }
+}
