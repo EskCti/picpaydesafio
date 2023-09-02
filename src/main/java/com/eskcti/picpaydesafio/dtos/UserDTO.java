113ac882411fc6da2835dc3735c3eb3e76f4977e
eskokado <eskokado@gmail.com>
2023-09-02 12:28:26 -0300

    feat: create UserDTO

---
+++ src/main/java/com/eskcti/picpaydesafio/dtos/UserDTO.java
@@ -0,0 +1,17 @@
+package com.eskcti.picpaydesafio.dtos;
+
+import com.eskcti.picpaydesafio.domain.user.UserType;
+
+import java.math.BigDecimal;
+
+public record UserDTO(
+    String firstName,
+    String lastName,
+    String document,
+    BigDecimal balance,
+    String email,
+    String password,
+    UserType userType
+) {
+
+}
